48d67a8592c49319aedcc66191e65c069ea11b4c
aryanbaranwal001 <aryanbaranwal131214@gmail.com>
2025-06-15 20:21:11 +0530

    did first and second opcodes

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,4 +1,6 @@
 use primitive_types::U256;
+use hex::ToHex;
+
 
 pub struct EvmResult {
     pub stack: Vec<U256>,
@@ -6,7 +8,7 @@ pub struct EvmResult {
 }
 
 pub fn evm(_code: impl AsRef<[u8]>) -> EvmResult {
-    let stack: Vec<U256> = Vec::new();
+    let mut stack: Vec<U256> = Vec::new();
     let mut pc = 0;
 
     let code = _code.as_ref();
@@ -17,10 +19,45 @@ pub fn evm(_code: impl AsRef<[u8]>) -> EvmResult {
 
         if opcode == 0x00 {
             // STOP
+            break;
         }
-    }
 
-    // TODO: Implement me
+        if opcode == 0x5f {
+            // PUSH0
+            stack.push(U256([0, 0, 0, 0]));
+        }
+
+        if opcode == 0x60 {
+            println!("Code in hex: {}", code.encode_hex::<String>());
+            
+        }
+
+
+
+
+
+
+        // if opcode == 0x60 {
+        //     // PUSH0
+        //     stack.push(U256([0, 0, 0, 0]));
+        // }
+        // if opcode == 0x5f {
+        //     // PUSH0
+        //     stack.push(U256([0, 0, 0, 0]));
+        // }
+        // if opcode == 0x5f {
+        //     // PUSH0
+        //     stack.push(U256([0, 0, 0, 0]));
+        // }
+        // if opcode == 0x5f {
+        //     // PUSH0
+        //     stack.push(U256([0, 0, 0, 0]));
+        // }
+        // if opcode == 0x5f {
+        //     // PUSH0
+        //     stack.push(U256([0, 0, 0, 0]));
+        // }
+    }
 
     return EvmResult {
         stack: stack,
